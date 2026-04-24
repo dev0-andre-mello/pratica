44dcf905b2d76684c70cbe702b8b321696ed4da6
dev0-andre-mello <punchmchn578@hotmail.com>
2026-04-24 15:54:19 -0300

    C# / .NET introduction

---
+++ c_sharp_test/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Digite seu nome: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Seja muito bem vindo(a), {name}!");
+Console.Write("Digite o ano do seu nascimento: ");
+int year = int.Parse(Console.ReadLine());
+int age = 2026 - year;
+Console.WriteLine($"Você tem {age} anos.");
+
+if (age >= 18)
+{
+    Console.WriteLine("Você é maior de idade");
+
+}
+else
+{
+    Console.WriteLine("Você é menor de idade");
+}
